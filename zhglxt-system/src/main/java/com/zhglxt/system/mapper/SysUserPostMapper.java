acbd7f1158e0ae8adb7052fe1d0e23983ae20a92
淡蓝色的忧伤 <1195465270@qq.com>
2024-01-19 23:57:03 +0800

    1. 系统升级到v4.7.8 2. 升级jquery到最新版v3.7.1 3. 通用detail详细信息弹窗不显示按钮 4. 升级druid到最新版本1.2.20 5. 用户列表新增抽屉效果详细信息 6. 升级layui到最新版本v2.8.18 7. 升级layer到最新版本v3.7.0 8. 角色列表显示数据权限 9. 升级oshi到最新版本6.4.11 10. 升级pagehelper到最新版1.4.7 11. 升级shiro到最新版本1.13.0 12. 重置密码鼠标按下显示密码 13. 优化数字金额大写转换精度丢失问题

--- zhglxt-system/src/main/java/com/zhglxt/system/mapper/SysUserPostMapper.java
+++ zhglxt-system/src/main/java/com/zhglxt/system/mapper/SysUserPostMapper.java
@@ -1,35 +1,38 @@
 package com.zhglxt.system.mapper;
 
 import com.zhglxt.system.entity.SysUserPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 /**
- * 用户与岗位关联表 数据层
- * 
+ * 用户与岗位 表 数据层
+ *
  * @author ruoyi
  */
-public interface SysUserPostMapper
-{
+@Mapper
+@Component
+public interface SysUserPostMapper {
     /**
      * 通过用户ID删除用户和岗位关联
-     * 
+     *
      * @param userId 用户ID
      * @return 结果
      */
     public int deleteUserPostByUserId(String userId);
-    
+
     /**
      * 通过岗位ID查询岗位使用数量
-     * 
+     *
      * @param postId 岗位ID
      * @return 结果
      */
     public int countUserPostById(String postId);
-    
+
     /**
      * 批量删除用户和岗位关联
-     * 
+     *
      * @param ids 需要删除的数据ID
      * @return 结果
      */
@@ -37,8 +40,8 @@ public interface SysUserPostMapper
 
     /**
      * 批量新增用户岗位信息
-     * 
-     * @param userPostList 用户角色列表
+     *
+     * @param userPostList 用户岗位列表
      * @return 结果
      */
     public int batchUserPost(List<SysUserPost> userPostList);
