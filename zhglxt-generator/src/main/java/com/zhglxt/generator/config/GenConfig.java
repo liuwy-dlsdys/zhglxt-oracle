cb9f7e2d7a6b4f858a0aeb3318d54b4d2ae1c4da
鸿业远图 <1195465270@qq.com>
2024-12-28 19:11:37 +0800

    1、修复多选下拉框open导致页签空白问题(IBBP7A) 2、升级jquery.validate到最新版本v1.21.0 3、优化select2下拉框必填背景色无法清空问题(IBB9XN) 4、优化时间控件清除按钮样式问题(IBA1NL) 5、优化导入带标题文件关闭清理(IB9Q9U) 6、优化表格图片预览动态路径显示问题 7、列宽拖动长内容自适应显示

--- zhglxt-generator/src/main/java/com/zhglxt/generator/config/GenConfig.java
+++ zhglxt-generator/src/main/java/com/zhglxt/generator/config/GenConfig.java
@@ -7,67 +7,80 @@ import org.springframework.stereotype.Component;
 
 /**
  * 读取代码生成相关配置
- * 
+ *
  * @author ruoyi
  */
 @Component
 @ConfigurationProperties(prefix = "gen")
-@PropertySource(value = { "classpath:generator.yml" })
-public class GenConfig
-{
-    /** 作者 */
+@PropertySource(value = {"classpath:generator.yml"})
+public class GenConfig {
+    /**
+     * 作者
+     */
     public static String author;
 
-    /** 生成包路径 */
+    /**
+     * 生成包路径
+     */
     public static String packageName;
 
-    /** 自动去除表前缀，默认是false */
+    /**
+     * 自动去除表前缀
+     */
     public static boolean autoRemovePre;
 
-    /** 表前缀(类名不会包含表前缀) */
+    /**
+     * 表前缀
+     */
     public static String tablePrefix;
 
-    public static String getAuthor()
-    {
+    /** 是否允许生成文件覆盖到本地（自定义路径） */
+    public static boolean allowOverwrite;
+
+    public static String getAuthor() {
         return author;
     }
 
     @Value("${author}")
-    public void setAuthor(String author)
-    {
+    public void setAuthor(String author) {
         GenConfig.author = author;
     }
 
-    public static String getPackageName()
-    {
+    public static String getPackageName() {
         return packageName;
     }
 
     @Value("${packageName}")
-    public void setPackageName(String packageName)
-    {
+    public void setPackageName(String packageName) {
         GenConfig.packageName = packageName;
     }
 
-    public static boolean getAutoRemovePre()
-    {
+    public static boolean getAutoRemovePre() {
         return autoRemovePre;
     }
 
     @Value("${autoRemovePre}")
-    public void setAutoRemovePre(boolean autoRemovePre)
-    {
+    public void setAutoRemovePre(boolean autoRemovePre) {
         GenConfig.autoRemovePre = autoRemovePre;
     }
 
-    public static String getTablePrefix()
-    {
+    public static String getTablePrefix() {
         return tablePrefix;
     }
 
     @Value("${tablePrefix}")
-    public void setTablePrefix(String tablePrefix)
-    {
+    public void setTablePrefix(String tablePrefix) {
         GenConfig.tablePrefix = tablePrefix;
+    }
+
+    public static boolean isAllowOverwrite()
+    {
+        return allowOverwrite;
+    }
+
+    @Value("${allowOverwrite}")
+    public void setAllowOverwrite(boolean allowOverwrite)
+    {
+        GenConfig.allowOverwrite = allowOverwrite;
     }
 }
--- zhglxt-generator/src/main/java/com/zhglxt/generator/controller/GenController.java
+++ zhglxt-generator/src/main/java/com/zhglxt/generator/controller/GenController.java
@@ -1,22 +1,5 @@
 package com.zhglxt.generator.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.io.IOUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.authz.annotation.RequiresRoles;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.druid.DbType;
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
@@ -32,20 +15,34 @@ import com.zhglxt.common.enums.BusinessType;
 import com.zhglxt.common.utils.StringUtils;
 import com.zhglxt.common.utils.security.PermissionUtils;
 import com.zhglxt.common.utils.sql.SqlUtil;
+import com.zhglxt.generator.config.GenConfig;
 import com.zhglxt.generator.entity.GenTable;
 import com.zhglxt.generator.entity.GenTableColumn;
 import com.zhglxt.generator.service.IGenTableColumnService;
 import com.zhglxt.generator.service.IGenTableService;
+import org.apache.commons.io.IOUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 代码生成 操作处理
- * 
+ *
  * @author ruoyi
  */
 @Controller
 @RequestMapping("/tool/gen")
-public class GenController extends BaseController
-{
+public class GenController extends BaseController {
     private String prefix = "tool/gen";
 
     @Autowired
@@ -56,8 +53,7 @@ public class GenController extends BaseController
 
     @RequiresPermissions("tool:gen:view")
     @GetMapping()
-    public String gen()
-    {
+    public String gen() {
         return prefix + "/gen";
     }
 
@@ -67,8 +63,7 @@ public class GenController extends BaseController
     @RequiresPermissions("tool:gen:list")
     @PostMapping("/list")
     @ResponseBody
-    public TableDataInfo genList(GenTable genTable)
-    {
+    public TableDataInfo genList(GenTable genTable) {
         startPage();
         List<GenTable> list = genTableService.selectGenTableList(genTable);
         return getDataTable(list);
@@ -80,8 +75,7 @@ public class GenController extends BaseController
     @RequiresPermissions("tool:gen:list")
     @PostMapping("/db/list")
     @ResponseBody
-    public TableDataInfo dataList(GenTable genTable)
-    {
+    public TableDataInfo dataList(GenTable genTable) {
         startPage();
         List<GenTable> list = genTableService.selectDbTableList(genTable);
         return getDataTable(list);
@@ -93,8 +87,7 @@ public class GenController extends BaseController
     @RequiresPermissions("tool:gen:list")
     @PostMapping("/column/list")
     @ResponseBody
-    public TableDataInfo columnList(GenTableColumn genTableColumn)
-    {
+    public TableDataInfo columnList(GenTableColumn genTableColumn) {
         TableDataInfo dataInfo = new TableDataInfo();
         List<GenTableColumn> list = genTableColumnService.selectGenTableColumnListByTableId(genTableColumn);
         dataInfo.setRows(list);
@@ -107,8 +100,7 @@ public class GenController extends BaseController
      */
     @RequiresPermissions("tool:gen:list")
     @GetMapping("/importTable")
-    public String importTable()
-    {
+    public String importTable() {
         return prefix + "/importTable";
     }
 
@@ -116,8 +108,7 @@ public class GenController extends BaseController
      * 创建表结构
      */
     @GetMapping("/createTable")
-    public String createTable()
-    {
+    public String createTable() {
         return prefix + "/createTable";
     }
 
@@ -128,8 +119,7 @@ public class GenController extends BaseController
     @Log(title = "代码生成", businessType = BusinessType.IMPORT)
     @PostMapping("/importTable")
     @ResponseBody
-    public AjaxResult importTableSave(String tables)
-    {
+    public AjaxResult importTableSave(String tables) {
         String[] tableNames = Convert.toStrArray(tables);
         // 查询表信息
         List<GenTable> tableList = genTableService.selectDbTableListByNames(tableNames);
@@ -143,19 +133,15 @@ public class GenController extends BaseController
      */
     @RequiresPermissions("tool:gen:edit")
     @GetMapping("/edit/{tableId}")
-    public String edit(@PathVariable("tableId") Long tableId, ModelMap mmap)
-    {
+    public String edit(@PathVariable("tableId") Long tableId, ModelMap mmap) {
         GenTable table = genTableService.selectGenTableById(tableId);
         List<GenTable> genTables = genTableService.selectGenTableAll();
         List<CxSelect> cxSelect = new ArrayList<CxSelect>();
-        for (GenTable genTable : genTables)
-        {
-            if (!StringUtils.equals(table.getTableName(), genTable.getTableName()))
-            {
+        for (GenTable genTable : genTables) {
+            if (!StringUtils.equals(table.getTableName(), genTable.getTableName())) {
                 CxSelect cxTable = new CxSelect(genTable.getTableName(), genTable.getTableName() + '：' + genTable.getTableComment());
                 List<CxSelect> cxColumns = new ArrayList<CxSelect>();
-                for (GenTableColumn tableColumn : genTable.getColumns())
-                {
+                for (GenTableColumn tableColumn : genTable.getColumns()) {
                     cxColumns.add(new CxSelect(tableColumn.getColumnName(), tableColumn.getColumnName() + '：' + tableColumn.getColumnComment()));
                 }
                 cxTable.setS(cxColumns);
@@ -174,8 +160,7 @@ public class GenController extends BaseController
     @Log(title = "代码生成", businessType = BusinessType.UPDATE)
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(@Validated GenTable genTable)
-    {
+    public AjaxResult editSave(@Validated GenTable genTable) {
         genTableService.validateEdit(genTable);
         genTableService.updateGenTable(genTable);
         return AjaxResult.success();
@@ -185,8 +170,7 @@ public class GenController extends BaseController
     @Log(title = "代码生成", businessType = BusinessType.DELETE)
     @PostMapping("/remove")
     @ResponseBody
-    public AjaxResult remove(String ids)
-    {
+    public AjaxResult remove(String ids) {
         genTableService.deleteGenTableByIds(ids);
         return AjaxResult.success();
     }
@@ -195,20 +179,15 @@ public class GenController extends BaseController
     @Log(title = "创建表", businessType = BusinessType.OTHER)
     @PostMapping("/createTable")
     @ResponseBody
-    public AjaxResult create(String sql)
-    {
-        try
-        {
+    public AjaxResult create(String sql) {
+        try {
             SqlUtil.filterKeyword(sql);
             List<SQLStatement> sqlStatements = SQLUtils.parseStatements(sql, DbType.mysql);
             List<String> tableNames = new ArrayList<>();
-            for (SQLStatement sqlStatement : sqlStatements)
-            {
-                if (sqlStatement instanceof MySqlCreateTableStatement)
-                {
+            for (SQLStatement sqlStatement : sqlStatements) {
+                if (sqlStatement instanceof MySqlCreateTableStatement) {
                     MySqlCreateTableStatement createTableStatement = (MySqlCreateTableStatement) sqlStatement;
-                    if (genTableService.createTable(createTableStatement.toString()))
-                    {
+                    if (genTableService.createTable(createTableStatement.toString())) {
                         String tableName = createTableStatement.getTableName().replaceAll("`", "");
                         tableNames.add(tableName);
                     }
@@ -218,9 +197,7 @@ public class GenController extends BaseController
             String operName = Convert.toStr(PermissionUtils.getPrincipalProperty("loginName"));
             genTableService.importGenTable(tableList, operName);
             return AjaxResult.success();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
             return AjaxResult.error("创建表结构异常[" + e.getMessage() + "]");
         }
@@ -232,8 +209,7 @@ public class GenController extends BaseController
     @RequiresPermissions("tool:gen:preview")
     @GetMapping("/preview/{tableId}")
     @ResponseBody
-    public AjaxResult preview(@PathVariable("tableId") Long tableId) throws IOException
-    {
+    public AjaxResult preview(@PathVariable("tableId") Long tableId) throws IOException {
         Map<String, String> dataMap = genTableService.previewCode(tableId);
         return AjaxResult.success(dataMap);
     }
@@ -244,8 +220,7 @@ public class GenController extends BaseController
     @RequiresPermissions("tool:gen:code")
     @Log(title = "代码生成", businessType = BusinessType.GENCODE)
     @GetMapping("/download/{tableName}")
-    public void download(HttpServletResponse response, @PathVariable("tableName") String tableName) throws IOException
-    {
+    public void download(HttpServletResponse response, @PathVariable("tableName") String tableName) throws IOException {
         byte[] data = genTableService.downloadCode(tableName);
         genCode(response, data);
     }
@@ -257,8 +232,11 @@ public class GenController extends BaseController
     @Log(title = "代码生成", businessType = BusinessType.GENCODE)
     @GetMapping("/genCode/{tableName}")
     @ResponseBody
-    public AjaxResult genCode(@PathVariable("tableName") String tableName)
-    {
+    public AjaxResult genCode(@PathVariable("tableName") String tableName) {
+        if (!GenConfig.isAllowOverwrite())
+        {
+            return AjaxResult.error("【系统预设】不允许生成文件覆盖到本地");
+        }
         genTableService.generatorCode(tableName);
         return AjaxResult.success();
     }
@@ -270,8 +248,7 @@ public class GenController extends BaseController
     @Log(title = "代码生成", businessType = BusinessType.UPDATE)
     @GetMapping("/synchDb/{tableName}")
     @ResponseBody
-    public AjaxResult synchDb(@PathVariable("tableName") String tableName)
-    {
+    public AjaxResult synchDb(@PathVariable("tableName") String tableName) {
         genTableService.synchDb(tableName);
         return AjaxResult.success();
     }
@@ -283,8 +260,7 @@ public class GenController extends BaseController
     @Log(title = "代码生成", businessType = BusinessType.GENCODE)
     @GetMapping("/batchGenCode")
     @ResponseBody
-    public void batchGenCode(HttpServletResponse response, String tables) throws IOException
-    {
+    public void batchGenCode(HttpServletResponse response, String tables) throws IOException {
         String[] tableNames = Convert.toStrArray(tables);
         byte[] data = genTableService.downloadCode(tableNames);
         genCode(response, data);
@@ -293,8 +269,7 @@ public class GenController extends BaseController
     /**
      * 生成zip文件
      */
-    private void genCode(HttpServletResponse response, byte[] data) throws IOException
-    {
+    private void genCode(HttpServletResponse response, byte[] data) throws IOException {
         response.reset();
         response.setHeader("Content-Disposition", "attachment; filename=\"zhglxt.zip\"");
         response.addHeader("Content-Length", "" + data.length);
